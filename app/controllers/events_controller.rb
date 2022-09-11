b301d6f3c23a4294f46b04d790e2e168509a8d94
José Sérgio <sergiosilva@outlook.com>
2022-09-11 19:43:25 -0300

    Add Creator_ID to EventsDB

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@
 
 class EventsController < ApplicationController
   before_action :set_event, only: %i[show edit update destroy]
-  before_action :authenticate_user!, only: %i[new create edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
 
   # GET /events or /events.json
   def index
